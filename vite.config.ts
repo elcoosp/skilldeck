b737f628baef8c500c6ae3419e3365d7d7b9b30f
elcoosp <elcoosp@gmail.com>
2026-03-12 16:32:29 +0100

    feat: setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import path from 'node:path'
 import { lingui } from '@lingui/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 const host = process.env.TAURI_DEV_HOST
 
-export default defineConfig(async () => ({
+export default defineConfig(() => ({
   plugins: [
+    tailwindcss(),
     react({
       // Use SWC plugin for Lingui macros
       plugins: [['@lingui/swc-plugin', {}]]
